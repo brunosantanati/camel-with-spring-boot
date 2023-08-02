bccbf127a7b35b7fde1894e3cc4556c414702bc0
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-08-02 11:14:29 -0300

    Log date time (fixed)

--- my-code/camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/a/MyFirstTimerRouter.java
+++ my-code/camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/a/MyFirstTimerRouter.java
@@ -3,13 +3,16 @@ package com.in28minutes.microservices.camelmicroservicea.routes.a;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 @Component
 public class MyFirstTimerRouter extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
         from("timer:first-timer")
-                .transform().constant("My Constant Message")
+                //.transform().constant("My Constant Message")
+                .transform().constant("Time now is " + LocalDateTime.now())
                 .to("log:first-timer");
     }
 

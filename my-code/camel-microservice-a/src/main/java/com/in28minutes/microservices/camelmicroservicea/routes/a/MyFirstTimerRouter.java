478cf96775108a67d9f591c18e8f73c4b607e1ee
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-08-02 11:10:35 -0300

    First Route with timer component

---
+++ my-code/camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/a/MyFirstTimerRouter.java
@@ -0,0 +1,15 @@
+package com.in28minutes.microservices.camelmicroservicea.routes.a;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyFirstTimerRouter extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        from("timer:first-timer")
+                .to("log:first-timer");
+    }
+
+}

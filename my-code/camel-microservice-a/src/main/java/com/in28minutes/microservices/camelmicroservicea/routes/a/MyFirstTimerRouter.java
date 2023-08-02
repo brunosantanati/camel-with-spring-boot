677dbdaa050ee44391c9cfb4f956b858adffd253
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-08-02 11:21:47 -0300

    Other ways to pass the Bean to bean()

--- my-code/camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/a/MyFirstTimerRouter.java
+++ my-code/camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/a/MyFirstTimerRouter.java
@@ -1,6 +1,7 @@
 package com.in28minutes.microservices.camelmicroservicea.routes.a;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -8,12 +9,17 @@ import java.time.LocalDateTime;
 @Component
 public class MyFirstTimerRouter extends RouteBuilder {
 
+    @Autowired
+    private GetCurrentTimeBean getCurrentTimeBean;
+
     @Override
     public void configure() throws Exception {
         from("timer:first-timer")
                 //.transform().constant("My Constant Message")
                 //.transform().constant("Time now is " + LocalDateTime.now())
-                .bean("getCurrentTimeBean")
+                //.bean("getCurrentTimeBean")
+                //.bean(getCurrentTimeBean, "getCurrentTime") //we can specify the method, but it's not necessary when the class has only one method
+                .bean(getCurrentTimeBean)
                 .to("log:first-timer");
     }
 

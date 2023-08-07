50be19d8a3a3b19cb3f78454ff6bfc9856f265c9
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-08-07 19:43:40 -0300

    Remove unnecessary code

--- my-code/camel-microservice-b/src/main/java/com/in28minutes/microservices/camelmicroserviceb/routes/ActiveMqXmlReceiverRouter.java
+++ my-code/camel-microservice-b/src/main/java/com/in28minutes/microservices/camelmicroserviceb/routes/ActiveMqXmlReceiverRouter.java
@@ -2,17 +2,10 @@ package com.in28minutes.microservices.camelmicroserviceb.routes;
 
 import com.in28minutes.microservices.camelmicroserviceb.CurrencyExchange;
 import org.apache.camel.builder.RouteBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 //@Component //comment it out to prevent generating a lot of logs
 public class ActiveMqXmlReceiverRouter extends RouteBuilder {
-
-    @Autowired
-    private MyCurrencyExchangeProcessor myCurrencyExchangeProcessor;
-
-    @Autowired
-    private MyCurrencyExchangeTransformer myCurrencyExchangeTransformer;
 
     @Override
     public void configure() throws Exception {

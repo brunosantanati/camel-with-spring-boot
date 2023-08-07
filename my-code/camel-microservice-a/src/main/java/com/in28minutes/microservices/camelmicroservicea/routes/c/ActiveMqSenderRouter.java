88f7e13fca1afd4119667d34cf0811c9cca08eaf
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-08-07 00:52:35 -0300

    Read file and post it in a queue

--- my-code/camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/c/ActiveMqSenderRouter.java
+++ my-code/camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/c/ActiveMqSenderRouter.java
@@ -7,8 +7,12 @@ import org.springframework.stereotype.Component;
 public class ActiveMqSenderRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
-        from("timer:active-mq-timer?period=10000")
+        /*from("timer:active-mq-timer?period=10000")
                 .transform().constant("My message for Active MQ")
+                .log("${body}")
+                .to("activemq:my-activemq-queue");*/
+
+        from("file:files/json")
                 .log("${body}")
                 .to("activemq:my-activemq-queue");
     }

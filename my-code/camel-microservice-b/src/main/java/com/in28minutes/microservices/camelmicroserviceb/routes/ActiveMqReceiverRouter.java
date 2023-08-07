ae90c819e76f703a7ae23d6b0c9d6e74a9145a95
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-08-07 01:00:37 -0300

    Unmarshall JSON to Java object

--- my-code/camel-microservice-b/src/main/java/com/in28minutes/microservices/camelmicroserviceb/routes/ActiveMqReceiverRouter.java
+++ my-code/camel-microservice-b/src/main/java/com/in28minutes/microservices/camelmicroserviceb/routes/ActiveMqReceiverRouter.java
@@ -1,6 +1,8 @@
 package com.in28minutes.microservices.camelmicroserviceb.routes;
 
+import com.in28minutes.microservices.camelmicroserviceb.CurrencyExchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,6 +10,7 @@ public class ActiveMqReceiverRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("activemq:my-activemq-queue")
+                .unmarshal().json(JsonLibrary.Jackson, CurrencyExchange.class)
                 .to("log:received-message-from-active-mq");
     }
 }

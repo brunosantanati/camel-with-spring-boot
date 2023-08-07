19529d14c15fb8cce4d6a9bceac91a468223b1f9
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-08-07 10:54:39 -0300

    Use choice(), when(), simple() and otherwise()

--- my-code/camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/b/MyFileRouter.java
+++ my-code/camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/b/MyFileRouter.java
@@ -3,11 +3,18 @@ package com.in28minutes.microservices.camelmicroservicea.routes.b;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
-//@Component //comment this out to prevent this route being used and use another instead
+@Component //comment this out to prevent this route being used and use another instead
 public class MyFileRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("file:files/input")
+                .routeId("Files-Input-Route")
+                .choice()
+                    .when(simple("${file:ext} ends with 'xml'"))
+                        .log("XML FILE")
+                    .otherwise()
+                        .log("Not an XML FILE")
+                .end()
                 .log("${body}")
                 .to("file:files/output");
     }

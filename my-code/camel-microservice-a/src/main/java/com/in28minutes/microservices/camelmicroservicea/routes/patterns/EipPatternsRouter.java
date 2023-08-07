cd379b27915b83ee310ee4c24a1e033d7bcf33a4
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-08-07 19:56:39 -0300

    Splitter Pattern: split by comma

--- my-code/camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/patterns/EipPatternsRouter.java
+++ my-code/camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/patterns/EipPatternsRouter.java
@@ -13,12 +13,19 @@ public class EipPatternsRouter extends RouteBuilder {
                 .multicast()
                 .to("log:something1", "log:something2", "log:something3");*/
 
-        // Splitter Pattern
-        from("file:files/csv")
+        // Splitter Pattern - example 1
+        /*from("file:files/csv")
                 .unmarshal().csv()
                 .split(body())
                 //.to("log:split-files");
                 .transform().body(String.class) //I needed to put this line as a way around the RuntimeCamelException
+                .log("${body}")
+                .to("activemq:split-queue");*/
+
+        // Splitter Pattern - example 2
+        from("file:files/csv")
+                .convertBodyTo(String.class)
+                .split(body(), ",") //split by comma
                 .log("${body}")
                 .to("activemq:split-queue");
     }

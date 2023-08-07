3bca854a429610a1d8c15c8f6f6777e64692718b
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-08-07 20:03:05 -0300

    Use a custom splitter

--- my-code/camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/patterns/EipPatternsRouter.java
+++ my-code/camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/patterns/EipPatternsRouter.java
@@ -1,10 +1,16 @@
 package com.in28minutes.microservices.camelmicroservicea.routes.patterns;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class EipPatternsRouter extends RouteBuilder {
+
+    @Autowired
+    SplitterComponent splitter;
     @Override
     public void configure() throws Exception {
 
@@ -23,10 +29,25 @@ public class EipPatternsRouter extends RouteBuilder {
                 .to("activemq:split-queue");*/
 
         // Splitter Pattern - example 2
-        from("file:files/csv")
+        /*from("file:files/csv")
                 .convertBodyTo(String.class)
                 .split(body(), ",") //split by comma
                 .log("${body}")
+                .to("activemq:split-queue");*/
+
+        // Splitter Pattern - example 3 using a custom splitter
+        from("file:files/csv")
+                .convertBodyTo(String.class)
+                .split(method(splitter))
+                .log("${body}")
                 .to("activemq:split-queue");
+    }
+}
+
+@Component
+class SplitterComponent {
+    public List<String> splitInput(String body) {
+        //here we would process the body and split it, but let's just return a random list
+        return List.of("ABC", "DEF", "GHI");
     }
 }

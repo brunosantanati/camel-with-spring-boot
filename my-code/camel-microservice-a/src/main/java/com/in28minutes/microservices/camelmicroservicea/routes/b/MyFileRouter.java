fe866287d0fecea8ffc0569c25ee8503f044f8f7
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-08-07 10:59:01 -0300

    Check if body contains a specific word

--- my-code/camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/b/MyFileRouter.java
+++ my-code/camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/b/MyFileRouter.java
@@ -9,9 +9,12 @@ public class MyFileRouter extends RouteBuilder {
     public void configure() throws Exception {
         from("file:files/input")
                 .routeId("Files-Input-Route")
+                .transform().body(String.class)
                 .choice()
                     .when(simple("${file:ext} ends with 'xml'"))
                         .log("XML FILE")
+                    .when(simple("${body} contains 'USD'"))
+                        .log("Not an XML FILE BUT contains USD")
                     .otherwise()
                         .log("Not an XML FILE")
                 .end()

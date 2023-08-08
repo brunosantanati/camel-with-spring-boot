02917526604f234aafeb50f8ab6f314add55a36b
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-08-08 11:17:15 -0300

    Routing Slip Enterprise Integration Pattern Example

--- my-code/camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/patterns/EipPatternsRouter.java
+++ my-code/camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/patterns/EipPatternsRouter.java
@@ -67,12 +67,28 @@ public class EipPatternsRouter extends RouteBuilder {
                 .to("activemq:split-queue");*/
 
         // Aggregation Enterprise Integration Pattern
-        from("file:files/aggregate-json")
+        /*from("file:files/aggregate-json")
                 .unmarshal().json(JsonLibrary.Jackson, CurrencyExchange.class)
                 .aggregate(simple("${body.to}"), new ArrayListAggregationStrategy())
                 .completionSize(3)
                 //.completionTimeout(HIGHEST)
-                .to("log:aggregate-json");
+                .to("log:aggregate-json");*/
+
+        // Routing Slip Enterprise Integration Pattern
+        String routingSlip = "direct:endpoint1, direct:endpoint3";
+
+        from("timer:routingSlip?period=10000")
+                .transform().constant("My message is hardcoded")
+                        .routingSlip(simple(routingSlip));
+
+        from("direct:endpoint1")
+                .to("log:direct-endpoint1");
+
+        from("direct:endpoint2")
+                .to("log:direct-endpoint2");
+
+        from("direct:endpoint3")
+                .to("log:direct-endpoint3");
     }
 }
 

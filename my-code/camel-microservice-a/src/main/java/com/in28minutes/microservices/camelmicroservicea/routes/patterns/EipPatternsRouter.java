b8432b8d52b6d13ee2504c0cdeb7bd97bdd6dfbf
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-08-08 12:47:29 -0300

    Dynamic Routing Enterprise Integration Pattern Example

--- my-code/camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/patterns/EipPatternsRouter.java
+++ my-code/camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/patterns/EipPatternsRouter.java
@@ -1,21 +1,27 @@
 package com.in28minutes.microservices.camelmicroservicea.routes.patterns;
 
 import com.in28minutes.microservices.camelmicroservicea.CurrencyExchange;
-import org.apache.camel.AggregationStrategy;
-import org.apache.camel.Exchange;
+import org.apache.camel.*;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class EipPatternsRouter extends RouteBuilder {
 
     @Autowired
     SplitterComponent splitter;
+
+    @Autowired
+    DynamicRouterBean dynamicRouterBean;
+
     @Override
     public void configure() throws Exception {
 
@@ -48,12 +54,12 @@ public class EipPatternsRouter extends RouteBuilder {
                 .to("activemq:split-queue");*/
 
         // Aggregation Enterprise Integration Pattern
-        from("file:files/aggregate-json")
+        /*from("file:files/aggregate-json")
                 .unmarshal().json(JsonLibrary.Jackson, CurrencyExchange.class)
                 .aggregate(simple("${body.to}"), new ArrayListAggregationStrategy())
                 .completionSize(3)
                 //.completionTimeout(HIGHEST)
-                .to("log:aggregate-json");
+                .to("log:aggregate-json");*/
 
         // Routing Slip Enterprise Integration Pattern
         /*String routingSlip = "direct:endpoint1, direct:endpoint3";
@@ -70,6 +76,20 @@ public class EipPatternsRouter extends RouteBuilder {
 
         from("direct:endpoint3")
                 .to("log:direct-endpoint3");*/
+
+        // Dynamic Routing Enterprise Integration Pattern
+        from("timer:dynamicRouting?period=10000")
+                .transform().constant("My message is hardcoded")
+                .dynamicRouter(method(dynamicRouterBean));
+
+        from("direct:endpoint1")
+                .to("log:direct-endpoint1");
+
+        from("direct:endpoint2")
+                .to("log:direct-endpoint2");
+
+        from("direct:endpoint3")
+                .to("log:direct-endpoint3");
     }
 }
 
@@ -78,5 +98,30 @@ class SplitterComponent {
     public List<String> splitInput(String body) {
         //here we would process the body and split it, but let's just return a random list
         return List.of("ABC", "DEF", "GHI");
+    }
+}
+
+@Component
+class DynamicRouterBean {
+    Logger logger = LoggerFactory.getLogger(DynamicRouterBean.class);
+
+    int invocations;
+
+    public String decideTheNextEndpoint(
+            @ExchangeProperties Map<String, String> properties,
+            @Headers Map<String, String> headers,
+            @Body String body
+    ) {
+        logger.info("{} {} {}", properties, headers, body);
+
+        invocations++;
+
+        if(invocations % 3 == 0)
+            return "direct:endpoint1";
+
+        if(invocations % 3 == 1)
+            return "direct:endpoint2, direct:endpoint3";
+
+        return null;
     }
 }
